6d507340d0f4f25b02cc9da1c7a8598f45d7ddb9
Gabrielle20 <gabriellecaeiro@gmail.com>
2022-08-11 11:54:48 +0200

    single product css

---
+++ public/js/cart/cart.js
@@ -0,0 +1,16 @@
+function addToCart(id){
+    $.ajax({
+        url:        '/produits/cart/add/'+id,
+        type:       'POST',
+        dataType:   'json',
+        async:      true,
+        data : {id: id},
+
+        success: function(data, status) {
+            notification(data.type, data.message);
+        },
+        error : function(xhr, textStatus, errorThrown) {
+            alert('Ajax request failed.');
+        }
+    });
+}
