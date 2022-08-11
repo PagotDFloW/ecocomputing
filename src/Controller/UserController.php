1386930116338b97f5549f42d637a921c6a7b086
PagotDFloW <webflopgt@gmail.com>
2022-08-11 23:16:30 +0200

    ajout liste des utilisateurs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Knp\Component\Pager\PaginatorInterface;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
@@ -33,6 +37,38 @@ class UserController extends AbstractController
     {
         $this->addFlash('success','Déconnexion réussie.');
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    //get all user with pagination
+    /**
+     * @Route("/admin/users", name="admin_users")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function users(PaginatorInterface $paginator, Request $request)
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        $pagination = $paginator->paginate(
+            $users, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            50/*limit per page*/
+        );
+        return $this->render('back/user/users.html.twig', [
+            'users' => $pagination
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}/delete", name="admin_delete_user")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function deleteUser($id): Response
+    {
+        $user = $this->getDoctrine()->getRepository(\App\Entity\User::class)->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($user);
+        $entityManager->flush();
+        $this->addFlash('success','Utilisateur supprimé.');
+        return $this->redirectToRoute('admin_users');
     }
 
 }
