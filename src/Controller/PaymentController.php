524e8e2cbde5e7ac89349cb9fca80b323f8bee95
noubache <n.bache@ecole-ipssi.net>
2022-08-12 11:16:01 +0200

    paiement manque plus qu'à vider le panier

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -32,29 +32,7 @@ class PaymentController extends AbstractController
         $commande = $this->getDoctrine()->getRepository(Commandes::class)->find($id);
         $total = $commande->getTotal() * 100;
         $products = $commande->getProduits();
-        // $form = $this->createForm(PaymentType::class);
-        // $form->handleRequest($request);
-        // if ($form->isSubmitted() && $form->isValid()) {
-        //     $token = $request->request->get('stripeToken');
-        //     dump($token);
         dump($request->get('stripeToken'));
-
-        // \Stripe\Stripe::setApiKey("sk_test_51LVYdyJqbezmWdjP8Jl1SvwJ8ThIvSBILSNyRQkhRliSaH72qTnwrVGUlGNGP3vXpfsDgmVlhxTctvZqNO8tSJRI00woXfhWE6");
-        // \Stripe\Charge::create([
-        //     'amount' => $total,
-        //     'currency' => 'eur',
-        //     'description' => 'Premium blog access',
-        //     'source' => $request->get('stripeToken'),
-        //     // 'receipt_email' => $user->getEmail(),
-        // ]);
-        // $charge = $stripe->charges->create(array(
-        //     "amount" => $commande->getPrix(),
-        //     "currency" => "eur",
-        //     "source" => $request->get('stripeToken'),
-        //     "description" => "Charge for hshshsh"
-        // ));
-
-        // dump(intval($commande->getTotal()));
 
         $token = $request->get('stripeToken');
 
@@ -65,12 +43,10 @@ class PaymentController extends AbstractController
             "source" => 'tok_mastercard',
             "description" => "first"
         ));
-        return  $this->render('payment/checkout.html.twig', ['total' => $total, 'commande' => $commande]);
-
-        // }
-        // return $this->render('cart/payment.html.twig', [
-        //     'form' => $this->createForm(PaymentType::class),
-        //     'commande' => $commande
-        // ]);
+        if ($token) {
+            return $this->render('cart/cartValidation.html.twig', ['commande' => $commande]);
+        } else {
+            return  $this->render('payment/checkout.html.twig', ['total' => $total, 'commande' => $commande]);
+        }
     }
 }
